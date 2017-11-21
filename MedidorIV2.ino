d5d803ef2177782deb4f2aca27b5a652937eb6bd
Fer Moreno López <xerk.kun@gmail.com>
2017-11-21 10:03:36 -0700

    Update MedidorIV2.ino

--- MedidorIV2.ino
+++ MedidorIV2.ino
@@ -6,13 +6,16 @@ LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 //Variables
 int inV = 0;
 int inI = 1;
+int Out = 8;
 
-int lecturasV[10];
-int lecturasI[10];
+int lecturasV[25];
+int lecturasI[25];
 int val, j = 0, i = 0, totalV = 0, totalI = 0, V = 0, I = 0;
-float Vmax , Imax, promedioV = 0.0, promedioI = 0.0, R = 1.0;
+float Vmax , Imax, promedioV = 0.0, promedioI = 0.0, R = 0.1;
 
 void setup() {
+  pinMode(Out, OUTPUT);
+  digitalWrite(Out,LOW);
   Serial.begin(9600);
   lcd.begin(8, 2);
   lcd.clear();
@@ -22,19 +25,20 @@ void setup() {
   Timer1.attachInterrupt(LCD);
 
   //Inicializar el vector
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 25; i++)
     lecturasV[i] = 0;
   i = 0;
 
-  for (j = 0; j < 10; j++)
+  for (j = 0; j < 25; j++)
     lecturasI[j] = 0;
   j = 0;
 }
 
 
 void loop() {
+  
   while (j < 25) {
-
+    digitalWrite(Out,HIGH);
     //sustrae el total con la lectura inmediata anterior
     totalV = totalV - lecturasV[i];
     totalI = totalI - lecturasI[i];
@@ -49,10 +53,10 @@ void loop() {
 
     //calcula el promedio de las lecturas
     i = i + 1;
-    if (i >= 10) {
+    if (i >= 25) {
       i = 0;
-      promedioV = totalV / 10;
-      promedioI = totalI / 10;
+      promedioV = totalV / 25;
+      promedioI = totalI / 25;
 
       V = promedioV/204.6;
       I = promedioI/(204.6*R);
@@ -68,6 +72,7 @@ void loop() {
     Serial.print(",");
     Serial.println(I);
   }
+  digitalWrite(Out,LOW);
   Vmax = Vmax;
   Imax = Imax;
 }
